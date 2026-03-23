d5afa4a51630332e1fa5f2a88577b03ed7f0a4b9
Jade Sweet <voidstarjms@gmail.com>
2026-03-23 16:54:33 -0700

    Cleaned up input logic, increased roll deadzone

--- core/entities/player/player.gd
+++ core/entities/player/player.gd
@@ -186,34 +186,14 @@ func _process(delta: float) -> void:
 			debug_F5_tap = false
 	
 	# Longitudinal thrust
-	if Input.is_action_pressed("thrust_forward"):
-		$"..".demand_lon(Input.get_action_strength("thrust_forward"))
-	if Input.is_action_pressed("thrust_backward"):
-		$"..".demand_lon(-Input.get_action_strength("thrust_backward"))
-		
+	$"..".demand_lon(Input.get_axis("thrust_backward", "thrust_forward"))	
 	# Strafing
-	if Input.is_action_pressed("strafe_up"):
-		$"..".demand_vrt(Input.get_action_strength("strafe_up"))
-	if Input.is_action_pressed("strafe_down"):
-		$"..".demand_vrt(-Input.get_action_strength("strafe_down"))
-	if Input.is_action_pressed("strafe_left"):
-		$"..".demand_lat(-Input.get_action_strength("strafe_left"))
-	if Input.is_action_pressed("strafe_right"):
-		$"..".demand_lat(Input.get_action_strength("strafe_right"))
-		
+	$"..".demand_vrt(Input.get_axis("strafe_down", "strafe_up"))
+	$"..".demand_lat(Input.get_axis("strafe_left", "strafe_right"))
 	# Rotation
-	if Input.is_action_pressed("pitch_up"):
-		$"..".demand_pitch(Input.get_action_strength("pitch_up"))
-	if Input.is_action_pressed("pitch_down"):
-		$"..".demand_pitch(-Input.get_action_strength("pitch_down"))
-	if Input.is_action_pressed("roll_cw"):
-		$"..".demand_roll(Input.get_action_strength("roll_cw"))
-	if Input.is_action_pressed("roll_ccw"):
-		$"..".demand_roll(-Input.get_action_strength("roll_ccw"))
-	if Input.is_action_pressed("yaw_right"):
-		$"..".demand_yaw(Input.get_action_strength("yaw_right"))
-	if Input.is_action_pressed("yaw_left"):
-		$"..".demand_yaw(-Input.get_action_strength("yaw_left"))
+	$"..".demand_pitch(Input.get_axis("pitch_down", "pitch_up"))
+	$"..".demand_roll(Input.get_axis("roll_ccw", "roll_cw"))
+	$"..".demand_yaw(Input.get_axis("yaw_left", "yaw_right"))
 	
 	var dash_input = dash_tap_manager.step()
 	if dash_input != null:
